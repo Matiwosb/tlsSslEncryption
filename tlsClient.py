d47ca43a0f8c4b6c57faa700b311c70b9da7d550
Matiwosb <workubirbo@gmail.com>
2022-05-01 18:25:19 -0500

    made a change

--- tlsClient.py
+++ tlsClient.py
@@ -6,6 +6,9 @@ import ssl
 import threading
 import random
 import string
+import OpenSSL
+from datetime import datetime
+from pprint import pprint
 
 nickname = (''.join(random.choice(string.ascii_letters) for i in range(10))) + str(random.randint(0, 10000))
 
@@ -18,39 +21,44 @@ client_key = 'client.key'
 
 # nickname = (''.join(random.choice(string.ascii_letters) for i in range(10))) + str(random.randint(0, 10000))
 
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
-context.load_cert_chain(certfile=client_cert, keyfile=client_key)
-
+# context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert).wrap_socket(s, server_hostname=server_sni_hostname)
+# print(context.load_cert_chain(certfile=client_cert, keyfile=client_key))
+# print(context.load_cert_chain(certfile=client_cert, keyfile=client_key))
 
 # def receive():
 #     while True:
 #         try:
 #             message = client.re
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn = context.wrap_socket(s, server_side=False, server_hostname=server_sni_hostname)
-# conn.connect(('127.0.0.1', 8082))
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# conn = context.wrap_socket(s, server_side=False, server_hostname=server_sni_hostname)
+# conn = context.wrap_socket(s, server_hostname=server_sni_hostname)
+# conn.connect(('127.0.0.1', 44444))
+s.connect(('127.0.0.1', 44567))
 #  print("SSL established. Peer: {}".format(conn.getpeercert()))
+# s.getpeercert()
 
 def receive():
     while True:
         try:
-            message = conn.recv(1024).decode('ascii')
+            message = s.recv(1024).decode('ascii')
             # if message == '.exit':
             #     client.close()
             if message == 'NICK':
-                conn.send(nickname.encode('ascii'))
+                s.send(nickname.encode('ascii'))
             else:
                 print(message)
         except:
             print('An error occurred!')
-            conn.close()
+            s.close()
             break
 
 def write():
     while True:
-        print("SSL established. Peer: {}".format(conn.getpeercert()))
-        message = f'{nickname}: {input("")})'
-        conn.send(message.encode('ascii'))
+        # print("SSL established. Peer: {}".format(context.getpeercert()))
+        message = f'{nickname}: {input("")}'
+        s.send(message.encode('ascii'))
 # print("Sending: 'Hello, world!'")
 # conn.send(b"Hello, world!")
 # print("closing connection")
--- tlsServer.py
+++ tlsServer.py
@@ -1,37 +1,39 @@
-from email import message
-from http import client
+# from email import message
+# from http import client
 import socket
 from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SHUT_RDWR
 import ssl
 import threading
-import urllib
+import sys
+# import urllib
 
 # urllib.request.http.client
 
 listen_addr = '127.0.0.1'
-listen_port = 8082
+listen_port = 44567
 server_cert = 'server.crt'
 client_cert = 'client.crt'
 server_key = 'server.key'
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+# context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 # context.options &= ~ssl.OP_NO_SSLv3
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_cert_chain(certfile=server_cert, keyfile=server_key)
-context.load_verify_locations(cafile=client_cert)
+# context.verify_mode = ssl.CERT_REQUIRED
+# context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+# context.load_verify_locations(cafile=client_cert)
 
-bindsocket = socket.socket()
+bindsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 bindsocket.bind((listen_addr, listen_port))
-bindsocket.listen(5)
+bindsocket.listen()
 
 clientlist = []
 
+
 def deal_with_client(connstream):
-    data = connstream.recv(4096)
+    data = connstream.recv(1024)
 
     while data:
         print(data)
-        data = connstream.recv(4096)
+        data = connstream.recv(1024)
 
 
 def broadcast(message):
@@ -52,7 +54,7 @@ def endClientConnection(client):
 def handle(client):
     while True:
         try:
-            message = client.recv(4096)
+            message = client.recv(1024)
             broadcast(message)
             print(message.decode('asscii'))
             if message.decode('ascii').split(":")[1] == " .exit":
@@ -65,11 +67,12 @@ def receive():
     while True:
         print("Waiting for client")
         newsocket, fromaddr = bindsocket.accept()
-        print("Client connected: {}:{}".format(fromaddr[0], fromaddr[1]))
+        print(f"connected with {str(fromaddr)}")
+        # print("Client connected: {}:{}".format(fromaddr[0], fromaddr[1]))
 
-        client.send("NICK".encode('ascii'))
-        nickname = client.recv(4096).decode('ascii')
-        clientlist.append((client, nickname))
+        newsocket.send("NICK".encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        clientlist.append((newsocket, nickname))
 
         print(f'Nickname of the client is {nickname}')
         broadcast(f'{nickname} joined the chat!'.encode('ascii'))
@@ -82,12 +85,13 @@ def receive():
         threading = threading.Thread(target=handle, args=(client,))
         threading.start()
 
+'''
         conn = context.wrap_socket(newsocket, server_side=True)
         print("SSL established. Peer: {}".format(conn.getpeercert()))
         buf = b''  #Buffer to hold received client data
         try:
             while True:
-                data = conn.recv(4096)
+                data = conn.recv(1024)
                 if data:
                 # Client sent us data. Append to buffer
                     buf += data
@@ -99,5 +103,8 @@ def receive():
             print("Closing connection")
             conn.shutdown(socket.SHUT_RDWR)
             conn.close()
+'''
+        # threading = threading.Thread(target=handle, args=(client,))
+        # threading.start()
 
 receive()
